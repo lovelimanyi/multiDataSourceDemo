b316086640a319fcf4bd3a63dc79952ab6630fc0
lovelimanyi <1161859568@qq.com>
2019-06-08 18:59:52 +0800

    while（）要求boolean，不能int
            int x = 1, y = 6;
            while (y--) {
                x--;
            }

---
+++ src/test/java/com/xianjinxia/multiDatasource/mian_shi/cai_yi/WhileFoo.java
@@ -0,0 +1,17 @@
+package com.xianjinxia.multiDatasource.mian_shi.cai_yi;
+
+public class WhileFoo {
+    public static void main(String[] args) {
+//        int x = 1, y = 6;
+//        while (y--) {
+//            x--;
+//        }
+    }
+
+//    运行结果：
+//    Incompatible types.
+//    Required:
+//    boolean
+//    Found:
+//    int
+}
