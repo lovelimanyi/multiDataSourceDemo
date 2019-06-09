568464ad02c6a8fa4344e3e91221659978286f94
lovelimanyi <1161859568@qq.com>
2019-06-09 22:28:51 +0800

    echarts柱状图demo

---
+++ src/main/resources/echarts-drilldown-demo/js/return-button.js
@@ -0,0 +1,27 @@
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom);
+// var option = drillDown.getOption();
+// drillDown.initChart(myChart,option);
+initData();
+$('#return-button').on('click',function(){
+      var myChart = echarts.init(dom);
+      // var option = drillDown.getOption();
+      // drillDown.initChart(myChart,option);
+    initData();
+
+
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
